6600807899c3b216c88d8690669a9b785d4e0b0e
Jonathan Kaneshiro <jkaneshiro97@gmail.com>
2020-07-11 15:59:04 -0700

    top 50 hits

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import pprint
 
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+top50_uri = 'spotify:playlist:37i9dQZEVXbLRQDuF5jeBp'
 
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
+# today top hits store in dictionary
+
 
 def main():
-    results = spotify.artist_top_tracks(lz_uri)
+    results = spotify.playlist(top50_uri)
+    # print(results)
 
-    for track in results['tracks'][:10]:
-        print('track    : ' + track['name'])
-        print('audio    : ' + track['preview_url'])
-        print('cover art: ' + track['album']['images'][0]['url'])
+    for track in results:
+        print(track[0:10])
+        # print('audio    : ' + track['preview_url'])
+        # print('cover art: ' + track['album']['images'][0]['url'])
         print()
 
 if __name__ == "__main__":
